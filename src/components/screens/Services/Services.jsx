ea115d72d909b2820f6012ca788499fcdf45ae66
DmitrySergeevich <efos.ds@gmail.com>
2023-02-21 14:24:40 +0700

    section Services (mobile version

--- src/components/screens/Services/Services.jsx
+++ src/components/screens/Services/Services.jsx
@@ -14,12 +14,27 @@ const Services = () => {
   return (
     <Container>
       <div className={styles.wrap} id="about">
-        <div><img src={ParkingIcon} alt="" /><span><strong>120-130 мест</strong> причал-стоянка <br />на воде</span></div>
-        <div><img src={GarageIcon} alt="" /><span><strong>900 м<sup>2</sup></strong> теплый ангар <br /> для хранения</span></div>
-        <div><img src={ServicesIcon} alt="" /><span><strong>{'>'} 100 услуг</strong> сервис для лодок <br /> и моторов</span></div>
-        <div><img src={SlipIcon} alt="" /><span><strong>21 m x 6 m</strong> Оборудованный <br /> слип</span></div>
-        <div><img src={GasIcon} alt="" /><span><strong>1–ая в регионе </strong> Плавучая<br /> Заправочная <br /> станция</span></div>
-        <div><img src={WalkIcon} alt="" /><span><strong>Отдых</strong> Речные <br /> прогулки</span></div>
+        <div>
+          <img src={ParkingIcon} alt="" />
+          <span><strong>120-130 мест</strong> причал-стоянка <br />на воде</span>
+        </div>
+        <div>
+          <img src={GarageIcon} alt="" />
+          <span><strong>900 м<sup>2</sup></strong> теплый ангар <br /> для хранения</span>
+        </div>
+        <div>
+          <img src={ServicesIcon} alt="" />
+          <span><strong>{'>'} 100 услуг</strong> сервис для лодок <br /> и моторов</span>
+        </div>
+        <div>
+          <img src={SlipIcon} alt="" /><span><strong>21 m x 6 m</strong> Оборудованный <br /> слип</span>
+        </div>
+        <div>
+          <img src={GasIcon} alt="" /><span><strong>1–ая в регионе </strong> Плавучая<br /> Заправочная <br /> станция</span>
+        </div>
+        <div>
+          <img src={WalkIcon} alt="" /><span><strong>Отдых</strong> Речные <br /> прогулки</span>
+        </div>
       </div>
     </Container>
   );
